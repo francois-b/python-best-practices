7d4f9c29327c31b93d2dd977739107faa205ecdd
Francois <f.bouet@gmail.com>
2016-07-06 17:39:23 +0100

    Add a query function, move es object to top level

--- indexer.py
+++ indexer.py
@@ -8,21 +8,23 @@ import json
 
 import elasticsearch
 
+
+# Name of the index as a global variable
+INDEX_NAME = "supercharger"
+
+es = elasticsearch.Elasticsearch()
+
+
 def index():
 	"Send documents to Elasticsearch"
 
 	# Import data from the CSV file
 	csvreader = csv.DictReader(open("data.csv"))
 
-	# Name of the index as a global variable
-	INDEX_NAME = "supercharger"
-
 	# Turn the CSV data into a list of dictionaries
 	documents = []
 	for line in csvreader:
 		documents.append(line)
-
-	es = elasticsearch.Elasticsearch()
 
 	# If the index already exists, delete it
 	if INDEX_NAME in es.indices.stats()["indices"].keys():
@@ -36,5 +38,21 @@ def index():
 	for doc in documents:
 	    es.index(index=INDEX_NAME, doc_type="communication_item", body=doc)
 
+
+def query(text):
+	"Issue a simple text query to Elasticsearch."
+
+	body = {
+	    "query": {
+	        "fuzzy": {
+	            "body": text
+	        }
+	    }
+	}
+	results = es.search(index=INDEX_NAME, doc_type="communication_item",
+						body=body)
+	return results["hits"]["hits"]
+
+
 if __name__ == "__main__":
-	index()
+	print query("helvetica")
