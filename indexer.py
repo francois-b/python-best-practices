ef2e9ffc204c4bfd20d26f99e6a1d4878645d710
Francois <f.bouet@gmail.com>
2016-07-06 17:05:51 +0100

    Move the index metadata to a separate JSON file

--- indexer.py
+++ indexer.py
@@ -4,6 +4,7 @@ This module provides indexing capabilities to easily send text to ES.
 """
 
 import csv
+import json
 
 import elasticsearch
 
@@ -25,74 +26,7 @@ if INDEX_NAME in es.indices.stats()["indices"].keys():
 	es.indices.delete(index=INDEX_NAME)
 
 # Define the settings and mappings of our index
-index_settings_and_mappings = {
-    "settings": {
-        "index": {
-            "number_of_shards": 1,
-            "number_of_replicas": 0
-        },
-        "analysis": {
-            "analyzer": {
-                "multiple_word_analyzer": {
-                    "type": "custom",
-                    "tokenizer": "standard",
-                    "filter": ["multiple_word_filter"]
-                },
-                "complex_analyzer": {
-                    "type": "custom",
-                    "tokenizer": "standard",
-                    "filter": [
-                    	"lowercase", "stemmer", "my_synonym_filter", "stop",
-                    	"multiple_word_filter", "positional_fix", "trim",
-                    	"unique"
-                    ]
-                },
-                "complex_query_analyzer": {
-                    "type": "custom",
-                    "tokenizer": "keyword",
-                    "filter": ["multiple_word_filter"]
-                }
-            },
-            "filter": {
-                "multiple_word_filter": {
-                    "type": "shingle",
-                    "output_unigrams": True,
-                    "min_shingle_size": 2,
-                    "max_shingle_size": 10
-                },
-                "my_synonym_filter" : {
-                    "type" : "synonym",
-                    "synonyms" : [
-                        "ze => the",
-                        "all => the"
-                    ]
-                },
-                "positional_fix": {
-                    "type": "pattern_replace",
-                    "pattern": " ?_",
-                    "replace": ""
-                }
-            }
-        }
-    },
-    "mappings": {
-        "communication_item": {
-            "properties": {
-                "category": {
-                    "type": "string",
-                    "analyzer": "multiple_word_analyzer",
-                    "search_analyzer": "keyword"
-                },
-                "body": {
-                    "type": "string",
-                    "analyzer": "complex_analyzer",
-                    "search_analyzer": "complex_query_analyzer"
-                }
-            }
-        }
-    }
-}
-
+index_settings_and_mappings = json.load(open("index_metadata.json"))
 es.indices.create(index=INDEX_NAME, body=index_settings_and_mappings)
 
 # Index all documents from the CSV file
