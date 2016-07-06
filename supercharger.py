62fb02375b9c78133edd6af4cb3cd76703bca170
Francois <f.bouet@gmail.com>
2016-07-06 18:22:07 +0100

    Use context managers for opening files

--- supercharger.py
+++ supercharger.py
@@ -18,7 +18,8 @@ import elasticsearch
 
 
 # Load the configuration
-cfg = json.load(open("config.json"))
+with open("config.json") as config_file:
+	cfg = json.load(config_file)
 
 es = elasticsearch.Elasticsearch()
 
@@ -27,12 +28,13 @@ def index(index_name=cfg["index_name"], type_name=cfg["type_name"]):
 	"Send documents to Elasticsearch"
 
 	# Import data from the CSV file
-	csvreader = csv.DictReader(open("data.csv"))
+	with open("data.csv") as data_file:
+		csvreader = csv.DictReader(data_file)
 
-	# Turn the CSV data into a list of dictionaries
-	documents = []
-	for line in csvreader:
-		documents.append(line)
+		# Turn the CSV data into a list of dictionaries
+		documents = []
+		for line in csvreader:
+			documents.append(line)
 
 	# If the index already exists, delete it
 	if index_name in es.indices.stats()["indices"].keys():
