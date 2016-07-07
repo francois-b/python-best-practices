a051386b188e3ba752ddf58d93b2ebcab0545f86
Francois <f.bouet@gmail.com>
2016-07-07 09:50:12 +0100

    Add proper tests

--- test/test_supercharger.py
+++ test/test_supercharger.py
@@ -1,7 +1,26 @@
+import time
+
+import elasticsearch
+
 import supercharger
 
-def f():
-    return 3
 
-def test_function():
-    assert f() == 3
+def test_indexing():
+    es = elasticsearch.Elasticsearch()
+    supercharger.index(index_name="test-dummy-index")
+
+    # Wait for Elasticsearch to index the documents
+    time.sleep(1)
+
+    # Query for all documents
+    documents = es.search(index="test-dummy-index",
+                          body={"query": {"match_all": {}}})["hits"]["hits"]
+
+    # Make sure we get all documents sent
+    assert len(documents) == 8
+
+
+def test_query():
+    results = supercharger.query(text="coffee", index_name="test-dummy-index")
+    assert len(results) == 1
+    assert results[0]["_source"]["category"] == "email"
